0952f7bf71d62f192a4e07e97eca70c2cd50d817
nukkey <surouraihu15@gmail.com>
2020-04-28 09:39:00 +0900

    groupmessage

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -6,7 +6,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Choose what kind of storage to use for this uploader:
   storage :file
   # storage :fog
-  process resize_to_fit: [800,800]
+  
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
@@ -20,7 +20,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   #
   #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
   # end
-
+  process resize_to_fit: [250, 250]
   # Process files as they are uploaded:
   # process scale: [200, 300]
   #
