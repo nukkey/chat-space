49af701c986c1da5586cd105461823ba9dea8140
nukkey <surouraihu15@gmail.com>
2020-05-02 18:14:00 +0900

    自動更新機能の実装

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,7 +2,7 @@ $(function(){
   function buildHTML(message){
    if ( message.image ) {
      var html =
-      `<div class="MessageBox">
+      `<div class="MessageBox" data-message-id=${message.id}>
         <div class="NameBox">
           ${message.user_name}
           <div class="DateBox">
@@ -13,13 +13,13 @@ $(function(){
           <p class="InputText__content">
             ${message.content}
           </p>
+         <img src=${message.image} >
         </div>
-        <img src=${message.image} >
        </div>`
      return html;
    } else {
      var html =
-      `<div class="MessageBox">
+      `<div class="MessageBox" data-message-id=${message.id}>
         <div class="NameBox">
           ${message.user_name}
           <div class="DateBox">
@@ -35,28 +35,53 @@ $(function(){
      return html;
    };
   }
-$('#new_message').on('submit', function(e){
-  e.preventDefault();
-  var formData = new FormData(this);
-  var url = $(this).attr('action')
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action')
   
-  $.ajax({
-    url: url,
-    type: "POST",
-    data: formData,
-    dataType: 'json',
-    processData: false,
-    contentType: false
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.ChatMain__message').append(html);
+      $('form')[0].reset();
+      $('.ChatMain__message').animate({ scrollTop: $('.ChatMain__message')[0].scrollHeight});
+    })
+    .fail(function() {
+      alert('メッセージ送信に失敗しました');
+    });
+    return false;
   })
-   .done(function(data){
-     var html = buildHTML(data);
-     $('.ChatMain__message').append(html);
-     $('form')[0].reset();
-     $('.ChatMain__message').animate({ scrollTop: $('.ChatMain__message')[0].scrollHeight});
-   })
-   .fail(function() {
-    alert('メッセージ送信に失敗しました');
-   });
-  return false;
- })
+  var reloadMessages = function() {
+    var last_message_id = $('.MessageBox:last').data("message-id");
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+     if (messages.length !== 0) {
+      var insertHTML = '';
+      $.each(messages, function(i, message) {
+        insertHTML += buildHTML(message)
+      });
+      $('.ChatMain__message').append(insertHTML);
+      $('.ChatMain__message').animate({ scrollTop: $('.ChatMain__message')[0].scrollHeight} );
+     }
+    })
+    .fail(function() {
+      alert('error');
+    });
+  };
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
 });
